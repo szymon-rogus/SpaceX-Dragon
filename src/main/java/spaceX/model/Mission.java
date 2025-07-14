cbfdf4fdcc47fa293c8cee4b80b8f379205ab2dc
szymon-rogus <sszymek98@gmail.com>
2025-07-14 11:55:37 +0200

    Simple Mission implementation v. 1.0

---
+++ src/main/java/spaceX/model/Mission.java
@@ -0,0 +1,54 @@
+package spaceX.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import spaceX.exception.AssignmentException;
+import spaceX.status.MissionStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Mission {
+
+    private String name;
+
+    private MissionStatus status;
+
+    private List<Rocket> assignedRockets;
+
+    public Mission(String name) {
+        this.name = name;
+        status = MissionStatus.SCHEDULED;
+        assignedRockets = new ArrayList<>();
+    }
+
+    public void assignRocket(Rocket rocket) {
+        if (rocket.isAssigned()) {
+            throw new AssignmentException("ERROR: Rocket is already assigned to different mission!");
+        }
+        assignedRockets.add(rocket);
+        /// More logic?
+    }
+
+    public void assignRockets(List<Rocket> rockets) {
+        rockets.forEach(this::assignRocket);
+    }
+
+    public void unassignRocket(Rocket rocket) {
+        if (!assignedRockets.contains(rocket)) {
+            throw new AssignmentException("WARNING: Trying to unassign rocket which has not been assigned to this mission");
+        }
+        /// More logic?
+    }
+
+    public void unassignRocket(List<Rocket> rockets) {
+        rockets.forEach(this::unassignRocket);
+    }
+
+    private void updateStatus() {
+        /// TODO: multiple edge cases, method called on every assigned rocket change
+        /// Implement observer?
+    }
+}
