5f7dfae76e1ccf1b725a4b1a8d498bcd6023ca51
szymon-rogus <sszymek98@gmail.com>
2025-07-14 11:52:42 +0200

    RocketTest ready for simple Rocket implementation

--- src/test/java/spaceX/model/RocketTest.java
+++ src/test/java/spaceX/model/RocketTest.java
@@ -14,16 +14,24 @@ public class RocketTest {
     }
 
     @Test
-    public void tesNotAssigned() {
+    public void testNotAssigned() {
         Rocket rocket = new Rocket("Spruce");
 
         Assert.assertFalse(rocket.isAssigned());
     }
 
     @Test
-    public void testAssigned() {
+    public void testInSpaceAssigned() {
         Rocket rocket = new Rocket("Tiger");
         rocket.setStatus(RocketStatus.IN_SPACE);
+
+        Assert.assertTrue(rocket.isAssigned());
+    }
+
+    @Test
+    public void testInRepairAssigned() {
+        Rocket rocket = new Rocket("Tiger");
+        rocket.setStatus(RocketStatus.IN_REPAIR);
 
         Assert.assertTrue(rocket.isAssigned());
     }
